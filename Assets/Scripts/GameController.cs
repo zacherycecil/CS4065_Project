08ef1de9a35c1b0ccb0135d1a808d8eece9eca7a
Zachery Lewis <zcjl98@yahoo.ca>
2021-03-15 17:06:08 -0300

    added a few things

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public bool roundStarted;
+    public Text menuText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        roundStarted = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            roundStarted = true;
+        }
+
+        if (!roundStarted)
+        {
+            menuText.text = "Iain Campbell and Zachery Lewis Present: Duck Hunt\n\nPress space to start";
+        }
+        else
+        {
+            menuText.text = "";
+        }
+    }
+}
